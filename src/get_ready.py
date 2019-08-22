651b3bf6a5db45bde61213bf740eddc70289b3da
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-08-22 12:28:48 +0800

    fix

--- src/get_ready.py
+++ src/get_ready.py
@@ -1,7 +1,2 @@
 from subprocess import Popen
-Popen("wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-linux.tar.gz").wait()
-Popen("tar -xzvf pandoc-2.7.3-linux.tar.gz").wait()
-Popen("cd pandoc-2.7.3-linux.tar.gz").wait()
-Popen("./configure").wait()
-Popen("make").wait()
-Popen("make install").wait()
+Popen("sudo apt-get install pandoc").wait()
